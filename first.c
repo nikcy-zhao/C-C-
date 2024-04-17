266ed6127f0923816f408e582f4be677ad5d406b
命运的猫 <cat@mingyundemaodeMacBook-Pro.local>
2024-04-17 11:11:18 +0800

    修改忽略文件

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b;
+  int sum;
+  a = 1;
+  b = a * a;
+  sum = a + b;
+
+  printf("My favorite number is %d because it is first\n", sum);
+
+  return 0;
+}
