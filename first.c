8ddef8fa53bfc3b7ccccd80555802d51e278efaf
命运的猫 <cat@mingyundemaodeMacBook-Pro.local>
2024-04-19 23:15:28 +0800

    printf格式化输出

--- first.c
+++ first.c
@@ -1,13 +1,18 @@
+#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
+#define PI 3.14159265358
 
 int main(void) {
-  int a, b;
-  int sum;
-  a = 1;
-  b = a * a;
-  sum = a + b;
+  int a = 2;
+  int b = 13543546;
 
-  printf("My favorite number is %d because it is first\n", sum);
+  // %d表示输出一个int型数据
+  printf("d =%d\n", b);
+
+  // %nd，其中n可以是任意数字，表示输出的最小宽度
+  // %2d：表示最小宽度是2，如果小于2，则默认用空格代替空位
+  printf("2d =%2d\n", a);
 
   return 0;
 }
